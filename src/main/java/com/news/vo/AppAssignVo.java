c53e8e213f357e18f30b31c9d1ed5737d151cfdb
MrSuYuan <18031924099@163.com>
2019-11-29 18:33:59 +0800

    新增实体

---
+++ src/main/java/com/news/vo/AppAssignVo.java
@@ -0,0 +1,23 @@
+package com.news.vo;
+
+public class AppAssignVo {
+
+    private int upstreamType;
+    private int probability;
+
+    public int getUpstreamType() {
+        return upstreamType;
+    }
+
+    public void setUpstreamType(int upstreamType) {
+        this.upstreamType = upstreamType;
+    }
+
+    public int getProbability() {
+        return probability;
+    }
+
+    public void setProbability(int probability) {
+        this.probability = probability;
+    }
+}
